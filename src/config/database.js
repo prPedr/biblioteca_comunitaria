e3e980b698604addb23326fedd447cdc16961f90
prPedr <prpedro025@gmail.com>
2026-04-12 14:40:24 -0300

    Revisao: Reescrevendo o database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3"
 
 const db = new sqlite3.Database("biblioteca_db.sqlite", (err) => {
   if (err) {
-    console.log("Falha ao se conectar com o banco de dados.")
+    console.log("Erro ao se conectar com o banco de dados.")
   } else {
     console.log("Conectado ao banco de dados.")
   }
