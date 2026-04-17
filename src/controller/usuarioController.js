ac265bfc39bdd5211e7efc79356ba5b760ab2769
prPedr <prpedro025@gmail.com>
2026-04-17 20:27:34 -0300

    Criacao do usuarioController.js

---
+++ src/controller/usuarioController.js
@@ -0,0 +1,16 @@
+import usuarioServices from "../service/usuarioServices.js"
+
+const criarUsuarioController = async (request, response) => {
+   const novoUsuario = await request.body
+
+   try {
+    const usuario = usuarioServices.criarUsuarioServices(novoUsuario)
+    response.status(201).send({usuario})
+   } catch (err) {
+    response.status(400).send(err.message)
+   }
+}
+
+export default {
+  criarUsuarioController
+}
