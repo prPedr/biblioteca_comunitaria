2e3dc40dcbd2bfb0336c86f419a0e8a1dad2d9e9
prPedr <prpedro025@gmail.com>
2026-04-19 18:08:22 -0300

    Revisao: Reescrevendo usuarioController.js

--- src/controller/usuarioController.js
+++ src/controller/usuarioController.js
@@ -7,7 +7,7 @@ const criarUsuarioController = async (request, response) => {
     const usuario = usuarioServices.criarUsuarioServices(novoUsuario)
     response.status(201).send({usuario})
   } catch (err) {
-    response.status(400).send(err.message)
+    response.status(400).send(err)
   }
 }
 
