89bbf63c7b54d85e6537e8910bc0ec851f6202dc
prPedr <prpedro025@gmail.com>
2026-07-06 22:19:50 -0300

    Inicio do desenvolvimento do front end da pagina de cadastro de usuarios

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from "cors"
 import usuarioRoutes from "./src/routes/usuarioRoutes.js"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(usuarioRoutes)
 
