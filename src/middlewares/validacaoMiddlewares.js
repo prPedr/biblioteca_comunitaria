d92a05cbe9ece4aeb5d0806596371f98c754e58a
prPedr <prpedro025@gmail.com>
2026-07-06 22:05:42 -0300

    Nova validacao do validacaoMiddlewares.js para remocao do zod

--- src/middlewares/validacaoMiddlewares.js
+++ src/middlewares/validacaoMiddlewares.js
@@ -1,24 +1,15 @@
 const validacao = (schema) => (request, response, next) => {
-  // O safeParse não "explode" a aplicação, ele retorna um objeto estruturado
-  const resultado = schema.safeParse(request.body)
+  const resultado = schema(request.body);
 
   if (!resultado.success) {
-    // Pegamos os erros diretamente de resultado.error.issues
-    const errosFormatados = resultado.error.issues.map((erro) => ({
-      campo: erro.path[0],
-      mensagem: erro.message
-    }))
-
     return response.status(400).json({
       Mensagem: "Erro de validação",
-      Detalhes: errosFormatados
-    })
+      Detalhes: resultado.erros
+    });
   }
 
-  // Se passou na validação, garantimos que o body use os dados validados pelo Zod 
-  // (útil se você usar transformações no schema depois)
-  request.body = resultado.data
-  next()
-}
+  request.body = resultado.data;
+  next();
+};
 
-export default validacao
+export default validacao;
